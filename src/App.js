821e4c7b0340ce0d601eb43fc1bd7dac559a540c
Jesse Tomchak <jtomchak@gmail.com>
2021-05-07 11:21:57 -0700

    More Changes 3

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          More changes
+          More changes Here is a bit more
         </a>
       </header>
     </div>
